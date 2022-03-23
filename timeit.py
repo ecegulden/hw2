cd5a2c742408f059f851d1fbbb4dd8f8d5af138e
ecegulden <99769370+ecegulden@users.noreply.github.com>
2022-03-22 23:21:55 -0700

    Update timeit.py

--- timeit.py
+++ timeit.py
@@ -4,5 +4,9 @@ import time
 #starting the fuction calculate_time
 
 def calculate_time(func):
-    def wrapper()
-    return 
+    def wrapper():
+        start= time.time()
+        func()
+        end = time.time()
+        print ("Total Time:, end-start)
+    return wrapper
