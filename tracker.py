bfc5dd39f4dd1c61c368d20814d53aa3f5c3aa9b
ecegulden <99769370+ecegulden@users.noreply.github.com>
2022-03-22 23:23:20 -0700

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,17 +1,7 @@
 #Create function called
-def func_counter(f): 
-    def wrapped(y):
-        wrapped.counter+= 1
-        return f
-    wrapped.counter = 0
-    return wrapped
-
-@func_counter 
-def foo(y):
-    return y+2**3-34
-    
-y=int(input("Enter:")) 
-foo(y)
-foo(y) 
-
-print(foo.counter) 
+def func_counter(func): 
+    def wrapper(*args):
+        wrapper.counter+= 1
+        func(*args)
+    wrapper.counter = 0
+    return wrapper
